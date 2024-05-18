cdd292abde2f83f4cfeafc3cf7bf47dbe229d61d
Marianna Mazzette <20104120@mail.wit.ie>
2024-05-18 10:36:35 +0100

    removed card layout

--- src/routes/report/+page.svelte
+++ src/routes/report/+page.svelte
@@ -2,7 +2,6 @@
   import { fleamarketService } from "$lib/services/fleamarket-service";
   import type { Fleamarket } from "$lib/types/fleamarket-types";
   import { currentSession, subTitle } from "$lib/stores";
-  import Card from "$lib/ui/Card.svelte";
   import FleamarketList from "$lib/ui/FleamarketList.svelte";
   import { onMount } from "svelte";
   import { get } from "svelte/store";
@@ -14,6 +13,37 @@
   });
 </script>
 
-<Card title="Fleamarkets">
-  <FleamarketList {fleamarkets} />
-</Card>
+<table class="table is-fullwidth">
+  <thead>
+    <th>Flea Market Name</th>
+    <th>Category</th>
+    <th>Country</th>
+    <th>Donor</th>
+  </thead>
+  <tbody>
+    {#each fleamarkets as fleamarket}
+      <tr>
+        <td>
+          {fleamarket.marketname}
+        </td>
+        <td>
+          {fleamarket.category}
+        </td>
+          <td>
+            {#if typeof fleamarket.country !== "string"}
+              {fleamarket.country.countryname}
+            {:else}
+              {fleamarket.country} 
+            {/if}
+          </td>
+        <td>
+          {#if typeof fleamarket.donor !== "string"}
+            {fleamarket.donor.lastName}, {fleamarket.donor.firstName}
+          {:else}
+            {fleamarket.donor}
+          {/if}
+        </td>
+      </tr>
+    {/each}
+  </tbody>
+</table>
